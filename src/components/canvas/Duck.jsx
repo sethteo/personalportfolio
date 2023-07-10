19a1dbed8c35df723a06e969e7c99cd927c64802
Hong Rui <hongruiseth.teo@gmail.com>
2023-07-10 09:59:55 +0800

    Added auto-rotation

--- src/components/canvas/Duck.jsx
+++ src/components/canvas/Duck.jsx
@@ -56,6 +56,8 @@ const DuckCanvas = () => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls 
+          autoRotate
+          autoRotateSpeed={0.5}
           enableZoom={false} 
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
